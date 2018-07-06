9c7dba928725a789e3b0bb3efbe14e4ef8b0f320
Ma'ayan <maayanlab@maayans-imac.anbg.mssm.edu>
2018-07-06 12:22:05 -0400

    l1000fwd result ids working

--- pipeline/scripts/OvarianCancer.py
+++ pipeline/scripts/OvarianCancer.py
@@ -11,10 +11,12 @@
 ########## 1. Load libraries
 #############################################
 ##### 1. Python modules #####
+from ruffus import *
 import sys
 import pandas as pd
 import numpy as np
 import scipy.stats as ss
+import json
 
 ##### 2. Custom modules #####
 # Pipeline running
@@ -25,68 +27,71 @@ import scipy.stats as ss
 ##### 1. Variables #####
 
 # TCGA expression normalized (fpkm) data file
-expfpkmfile = /Users/maayanlab/Documents/Ovarian Cancer Project/ovarian-cancer/rawdata/TCGA-OV-fpkm-uq.txt
+fpkm_file = "rawdata/TCGA-OV-fpkm-uq.txt"
 
-#######################################################
-#######################################################
-########## S1. Download Data and Make Dataframe
-#######################################################
-#######################################################
+########################################################
+########################################################
+########## S1. Finding Gene Signatures for Each Sample
+########################################################
+########################################################
 
-##### TCGA-fpkm expression data that is downloaded is made into a dataframe.
+##### TCGA-fpkm expression data that is downloaded is made into a dataframe and this is used to find gene signatures for each sample.
 ### Input: TCGA-fpkm expression data (.txt file)
-### Output: Dataframe of the TCGA expression data--the index is the gene symbol and the column headers are sample ids.
-
-#############################################
-########## 1. Creating dataframe
-#############################################
-
-@transform(input = expfpkmfile, output = /rawdata/df_expfpkm.csv)
-df_expfpkm = pd.read_table(expfpkmfile).set_index('gene_symbol')
-df_expfpkm.to_csv("/rawdata/df_expfpkm.csv")
-
-
-
-
-#######################################################
-#######################################################
-########## S2. Find Gene Signatures for Each Sample
-#######################################################
-#######################################################
-
-##### TCGA-fpkm expression dataframe is used to find the gene signature for each sample.
-### Input: Dataframe of the TCGA expression data--the index is the gene symbol and the column headers are sample ids.
 ### Output: Dictionary of the top 500 and bottom 500 genes for each sample
 
+
 ############################################################
-########## 1. Z-scores across rows and columns of dataframe
+########## 1. Reading in data and Z-scores across rows and columns of dataframe
 ############################################################
 
-@transform(input = /rawdata/df_expfpkm.csv, output = /rawdata/df_expfpkm_zscore.csv)
+# @follows(mkdir("s1-expression.dir")) ## may add subdirectories later
+
+@transform(fpkm_file,
+            suffix(".txt"),
+            "_zscore.txt")
 
 def zscore(infile, outfile):
-    infile = /rawdata/df_expfpkm.csv
-    # axis=0 is along rows
-    df_expfpkm_zscore = df_expfpkm.T.apply(ss.zscore, axis=0).T.dropna().apply(ss.zscore, axis=0)
-    outfile = df_expfpkm_zscore.to_csv('rawdata/df_expfpkm_zscore.csv')
 
+
+    # Read the infile using pandas
+    df_expfpkm = pd.read_table(infile).set_index('gene_symbol')
+
+    # perform the z-score normalization (axis=0 is along rows)
+    df_expfpkm_zscore = df_expfpkm.T.apply(ss.zscore, axis=0).T.dropna().apply(ss.zscore, axis=0)
+
+    # Save the Z-score normalized df to outfile
+    df_expfpkm_zscore.to_csv(outfile, sep='\t')
+
+    
 ############################################################
 ########## 2. Creating dictionary of gene signatures
 ############################################################
 
-@transform(input = /rawdata/df_expfpkm_zscore.csv, output = /rawdata/dict_signatures.txt)
+@transform(zscore, 
+            suffix('_zscore.txt'), 
+            "_signatures.json")
+
 
 def signatures(infile, outfile):
     
-    infile = /rawdata/df_expfpkm_zscore.csv
+    # create empty dictionary
+    dict_signatures = {}
+
+    df_expfpkm_zscore = pd.read_table(infile).set_index('gene_symbol')
     
-    # define empty dictionary
-    signatures = {}
     
+    # Get number of samples
+    n = len(df_expfpkm_zscore.columns)
+    i = 0
     
     # Loop through samples 
-    for sample in df_expfpkm_zscore.columns[5:]:
-        # Extract column
+    for sample in df_expfpkm_zscore.columns[:]:
+
+        # Print status
+        i += 1
+        print('Doing sample {sample} ({i}/{n})...'.format(**locals()))
+
+         # Extract column
         col = df_expfpkm_zscore[sample].sort_values(ascending=False) # sort values in decreasing order
         
         genesets = {
@@ -96,29 +101,150 @@ def signatures(infile, outfile):
             }
             
         # Extract the top 500 genes from the index
-        signatures[sample] = genesets
+        dict_signatures[sample] = genesets
 
-    # Save dictionary to .txt file
-    outfile = /rawdata/dict_signatures.txt
-    outfile = open("")
+    # open file
+    f = open(outfile, 'w')
+
+    # write to file
+    f.write(json.dumps(dict_signatures, ensure_ascii=False, indent=4))     
+    
+
+    # close file
+    f.close()
 
 
 
-#######################################################
-#######################################################
-########## S3. Find L1000fwd Reversing Drugs-result ids
-#######################################################
-#######################################################
 
-##### Gene signatures for each sample is used on L1000fwd to find result ids for samples for reversing drugs for signatures.
+######################################################################
+######################################################################
+########## S3. Find L1000fwd Reversing and Mimicking Drugs-result ids
+######################################################################
+######################################################################
+
+##### Gene signatures for each sample is used on L1000fwd to find result ids for samples for reversing and mimicking drugs for signatures.
 ### Input: Dictionary of the top 500 and bottom 500 genes for each sample
 ### Output: Dictionary of the L1000fwd reversing drug result ids for each sample
 
 ############################################################
-########## 1.
+########## 1. Reversing drug result ids
+############################################################
+
+@transform(signatures,
+            suffix('_signatures.json'),
+            "_L1000reversingresultids.json")
+
+
+def L1000reversingresultids(infile, outfile):
+
+
+    
+    # query the L1000 signatures using gene sets
+    import json, requests
+    from pprint import pprint
+
+    L1000FWD_URL = 'http://amp.pharm.mssm.edu/L1000FWD/'
+
+    # create empty dictionary for L1000FWD result ids
+    l1000fwd_reversing_resultids = {}
+
+    with open(infile) as infile2:
+        signature_data = json.load(infile2)
+
+    # Loop through dictionary
+    for sample, genesets in signature_data.items():
+
+        payload = {
+            # put downregulated genes in the upregulated spot to find drugs that reverse the signature
+            # same with upregulated gewnes in the downregulated spot
+            'up_genes': genesets['bottom'],
+            'down_genes': genesets['top']
+        }
+    
+        response = requests.post(L1000FWD_URL + 'sig_search', json=payload)
+        
+        if response.status_code == 200:
+            l1000fwd_reversing_resultids[sample] = response.json() #saving results to a dictionary
+            json.dump(response.json(), open('api3_result.json', 'w'), indent=4)      
+
+
+     # open file
+    f = open(outfile, 'w')
+
+    # write to file
+    f.write(json.dumps(l1000fwd_reversing_resultids, ensure_ascii=False, indent=4))
+
+    # close file
+    f.close()
+
+
+############################################################
+########## 2. Mimicking drug result ids
 ############################################################
 
 
+@transform(signatures,
+            suffix('_signatures.json'),
+            "_L1000mimickingresultids.json")
+
+
+def L1000mimickingresultids(infile, outfile):
+
+
+    
+    # query the L1000 signatures using gene sets
+    import json, requests
+    from pprint import pprint
+
+    L1000FWD_URL = 'http://amp.pharm.mssm.edu/L1000FWD/'
+
+    # create empty dictionary for L1000FWD result ids
+    l1000fwd_mimicking_resultids = {}
+
+    with open(infile) as infile2:
+        signature_data = json.load(infile2)
+
+    # Loop through dictionary
+    for sample, genesets in signature_data.items():
+
+        payload = {
+            # put downregulated genes in the upregulated spot to find drugs that reverse the signature
+            # same with upregulated gewnes in the downregulated spot
+            'up_genes': genesets['top'],
+            'down_genes': genesets['bottom']
+        }
+    
+        response = requests.post(L1000FWD_URL + 'sig_search', json=payload)
+        
+        if response.status_code == 200:
+            l1000fwd_mimicking_resultids[sample] = response.json() #saving results to a dictionary
+            json.dump(response.json(), open('api3_result.json', 'w'), indent=4)      
+
+
+     # open file
+    f = open(outfile, 'w')
+
+    # write to file
+    f.write(json.dumps(l1000fwd_mimicking_resultids, ensure_ascii=False, indent=4))
+
+    # close file
+    f.close()
+
+
+
+#######################################################
+#######################################################
+########## S4. L1000fwd Retrieve Top 50 Results
+#######################################################
+#######################################################
+
+##### Takes L1000fwd result ids for each sample and gives top 50 signature ids. Does this for both the reversing and mimicking drugs.
+### Input: Dictionary of L1000fwd result ids for each sample
+### Output: Dictionary of the L1000fwd top 50 signature ids for each sample.
+
+########################################################################
+########## 1. Top 50 signature ids for reversing drugs for each sample
+#######################################################################
 
 
 
@@ -128,4 +254,20 @@ def signatures(infile, outfile):
 
 
 
+
+
+
+
+#######################################################
+#######################################################
+########## S5. L1000fwd Get Single Signature by ID
+#######################################################
+#######################################################
+
+##### Takes L1000fwd top 50 signature ids for each sample and ouputs signature--use this to extract drug.
+### Input: Dictionary of the L1000fwd top 50 signatue ids for each sample.
+### Output: Dataframe of the 50 top drugs for each sample--index is drug
+############################################################
+########## 1. ??
+############################################################
 
